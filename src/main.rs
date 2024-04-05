d88c7314519461914ed6b96db534f25b04be106d
-ˏˋ Maple ˊˎ <git@kalitsune.net>
2024-04-05 20:18:24 +0200

    🌐 Basic website structure

---
+++ src/main.rs
@@ -0,0 +1,12 @@
+use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new()
+            .service(web::redirect("/", "http://github.com/fabricated-world/fabulous-server"))
+    })
+    .bind(("127.0.0.1", 8080))?
+    .run()
+    .await
+}
